016cbc6f3a5a99a2a0d6d2822ac783208a1d2af1
wilsann <wildanarif268@gmail.com>
2022-09-08 15:09:33 +0700

    upload projek vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+
+    <HeaderWil />
+    <HeroCarousel />
+    <WomanSec />
+    <InstaSec />
+    <PartnerSec />
+    <FooterSec />
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderWil from '@/components/HeaderWil.vue'
+import HeroCarousel from '@/components/HeroCarousel.vue'
+import WomanSec from '../components/WomanSec.vue'
+import InstaSec from '@/components/InstaSec.vue'
+import PartnerSec from '../components/PartnerSec.vue'
+import FooterSec from '@/components/FooterSec.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+    HeaderWil,
+    HeroCarousel,
+    WomanSec,
+    InstaSec,
+    PartnerSec,
+    FooterSec
+}
+}
+</script>
